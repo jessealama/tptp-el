ed8f96f0292742b9d3ac46b0064c7d5752c52e35
Jesse Alama <jesse.alama@gmail.com>
2011-07-14 12:01:04 -0700

    File metadata à la Drew Adams.
    Drew is a great elisp hacker.

--- tptp.el
+++ tptp.el
@@ -1,8 +1,19 @@
 ;;; tptp.el --- Work with TPTP files, theorem provers, and model finders
-
+;;
+;; Filename: tptp.el
+;; Description: Work with TPTP files, theorem provers, and model finders.
+;; Author: Jesse Alama (jesse.alama@gmail.com)
+;; Maintainer: Jesse Alama (jesse.alama@gmail.com)
+;; Copyright (C) 2011, Jesse Alama, all rights reserved.
+;; Keywords: TPTP, automated theorem prover, automated theorem proving,
+;;   ATP, LADR, prover9, eprover, vampire
+;; Compatibility: GNU Emacs: 23.x
+;; 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
 ;;; Commentary:
 ;; 
-;; Acknowledgements: 
+;; Acknowledgements:
 ;;
 ;; Ed Zalta provided the impetus for launching this package.
 
