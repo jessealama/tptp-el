508a179ce1d206d5443db1c1df00d6efa3600c17
Jesse Alama <jesse.alama@gmail.com>
2011-10-22 21:52:32 +0200

    minor variable and documentation cleanup

--- eprover-used-principles.sh
+++ eprover-used-principles.sh
@@ -1,6 +1,12 @@
 #!/bin/bash -
 
-grep --only-matching ' initial(.*$' $1 \
+proof=$1
+background_theory=$2  # we don't actually need this; the proof object
+		      # emitted by E contain all the information we
+		      # need, so there's no need to consult the
+		      # background theory from which the proof came
+
+grep --only-matching ' initial(.*$' $proof \
     | cut -f 2 -d ',' \
     | cut -f 1 -d ')' \
     | sed -e 's/^ //' \
