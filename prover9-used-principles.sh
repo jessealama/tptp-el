ca66a20b8ea521200919468444d5ed5d94ae46a9
Jesse Alama <jesse.alama@gmail.com>
2011-10-22 21:58:19 +0200

    Don't say that the conjecture(s) is (are) used.

--- prover9-used-principles.sh
+++ prover9-used-principles.sh
@@ -35,5 +35,15 @@ prover9_proof=$1;
 theory=$2
 for principle in `prover9_labels_and_answers $prover9_proof`; do
     grep --silent "^fof($principle," $theory > /dev/null 2>&1;
-    if [ $? -eq "0" ]; then echo $principle; fi
+    if [ $? -eq "0" ]; then
+	# Don't say that the conjecture was used.  Of course, it will
+	# in general be on the formulas employed in the course of the
+	# proof, but that's not the sense of 'used' that we mean.  We
+	# are interested only in what principles of the background
+	# theory are used.
+	grep --silent "^fof($principle,conjecture," $theory > /dev/null 2>&1;
+	if [ $? -ne "0" ]; then
+	    echo $principle;
+	fi
+    fi
 done
