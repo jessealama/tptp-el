2ce6438be2e52cf528ddd4cda518337dc437d509
Jesse Alama <jesse.alama@gmail.com>
2011-10-23 00:36:22 +0200

    strip comments and expand everything

--- eprover-unused-principles.sh
+++ eprover-unused-principles.sh
@@ -2,7 +2,7 @@
 
 proof=$1;
 theory_of_proof=$2;
-for formula in `tptp4X -umachine $theory_of_proof | cut -f 1 -d ',' | sed -e 's/fof(//'`; do
-    grep --silent " initial(.*, $formula)" $proof;
+for formula in `tptp4X -c -x -umachine $theory_of_proof | cut -f 1 -d ',' | sed -e 's/fof(//'`; do
+    grep --silent " initial(.*, $formula)" $proof > /dev/null 2>&1;
     if [ $? -ne "0" ]; then echo $formula; fi
 done
