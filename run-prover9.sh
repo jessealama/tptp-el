64d4b49dbb726120f89ca064416037bbc23fc3f7
Jesse Alama <jesse.alama@gmail.com>
2011-10-23 02:49:13 +0200

    use ulimit, and the parameters

--- run-prover9.sh
+++ run-prover9.sh
@@ -3,6 +3,6 @@
 theory=$1;
 timeout=$2;
 
-ulimit -t $2;
+ulimit -t $timeout;
 
-tptp_to_ladr < $1 | prover9 -x 2> /dev/null | prooftrans renumber
+tptp_to_ladr < $theory | prover9 -x 2> /dev/null | prooftrans renumber
