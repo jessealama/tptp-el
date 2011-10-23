5d46dff4ce918bf2e341c0ff51ab07bcff7a3e5e
Jesse Alama <jesse.alama@gmail.com>
2011-10-23 02:50:23 +0200

    more careful coverage for prover9

--- eprover-used-principles.sh
+++ eprover-used-principles.sh
@@ -1,13 +1,17 @@
 #!/bin/bash -
 
 proof=$1
-background_theory=$2  # we don't actually need this; the proof object
-		      # emitted by E contain all the information we
-		      # need, so there's no need to consult the
-		      # background theory from which the proof came
+background_theory=$2
 
-grep --only-matching ' initial(.*$' $proof \
-    | cut -f 2 -d ',' \
-    | cut -f 1 -d ')' \
-    | sed -e 's/^ //' \
-    | sort -u
+function used() {
+    grep --only-matching ' initial(.*$' $proof \
+	| cut -f 2 -d ',' \
+	| cut -f 1 -d ')' \
+	| sed -e 's/^ //' \
+	| sort -u;
+}
+
+for formula in `used`; do
+    grep --silent "fof($formula,conjecture," $background_theory > /dev/null 2>&1;
+    if [ $? -ne "0" ]; then echo $formula; fi
+done
