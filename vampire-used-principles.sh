94484108da500f671cfb00cdc5c5421f1db082d5
Jesse Alama <jesse.alama@gmail.com>
2011-10-23 02:49:36 +0200

    reformat

--- vampire-used-principles.sh
+++ vampire-used-principles.sh
@@ -3,5 +3,6 @@
 proof=$1
 theory=$2
 
-tptp4X -umachine $theory | grep ',conjecture,' | cut -f 1 -d ',' | sed -e 's/fof(//';
-grep --only-matching '\[input [^[]*\]' $1 | sed -e 's/\[input \(.*\)\]/\1/' | sort -u
+grep --only-matching '\[input [^[]*\]' $1 \
+    | sed -e 's/\[input \(.*\)\]/\1/' \
+    | sort -u
