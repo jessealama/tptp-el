63ccd7b6aca168ce6788dcd3c5974048771141b2
Jesse Alama <jesse.alama@gmail.com>
2011-10-23 16:39:10 +0200

    more sanity checking.

--- eprover-used-principles.sh
+++ eprover-used-principles.sh
@@ -3,6 +3,36 @@
 proof=$1
 background_theory=$2
 
+if [ -z $proof ]; then
+    echo "Usage: `basename $0` EPROVER-PROOF TPTP-BACKGROUND-THEORY";
+    exit 1;
+fi
+
+if [ -z $background_theory ]; then
+    echo "Usage: `basename $0` EPROVER-PROOF TPTP-BACKGROUND-THEORY";
+    exit 1;
+fi
+
+if [ ! -f $proof ]; then
+    echo "Error: the supplied eprover proof '$proof' is not a regular file";
+    exit 1;
+fi
+
+if [ ! -r $proof ]; then
+    echo "Error: the supplied eprover proof '$proof' is not readable";
+    exit 1;
+fi
+
+if [ ! -f $background_theory ]; then
+    echo "Error: the supplied background theory '$background_theory' is not a regular file";
+    exit 1;
+fi
+
+if [ ! -r $background_theory ]; then
+    echo "Error: the supplied background theory '$background_theory' is not readable";
+    exit 1;
+fi
+
 function used() {
     grep --only-matching ' initial(.*$' $proof \
 	| cut -f 2 -d ',' \
