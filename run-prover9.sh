a2b4aacb95f7ac8fb8420ba644278125bc113004
Jesse Alama <jesse.alama@gmail.com>
2011-10-23 17:48:00 +0200

    check argument to ulimit -t

--- run-prover9.sh
+++ run-prover9.sh
@@ -3,6 +3,7 @@
 theory=$1;
 timeout=$2;
 
-ulimit -t $timeout;
+ulimit -t $timeout \
+    || (echo "Error: '$timeout' is not an acceptable argument to ulimit -t." && exit 1);
 
 tptp_to_ladr < $theory | prover9 -x 2> /dev/null | prooftrans renumber
