75e2466d421a86bbe426c45c666ff3d4433169a7
Jesse Alama <jesse.alama@gmail.com>
2011-10-23 02:49:59 +0200

    sanity check

--- prover9-unused-principles.sh
+++ prover9-unused-principles.sh
@@ -1,8 +1,19 @@
 #!/bin/bash -
 
-proof=$1;
-theory_of_proof=$2;
-for formula in `tptp4X -umachine $theory | cut -f 1 -d ',' | sed -e 's/fof(//'`; do
-    grep --silent "label($formula)" $proof > /dev/null 2>&1;
+prover9_proof=$1;
+theory=$2;
+
+if [ -z $prover9_proof ]; then
+    echo "Usage: `basename $0` PROVER9-PROOF-OBJECT TPTP-BACKGROUND-THEORY";
+    exit 1;
+fi
+
+if [ -z $theory ]; then
+    echo "Usage: `basename $0` PROVER9-PROOF-OBJECT TPTP-BACKGROUND-THEORY";
+    exit 1;
+fi
+
+for formula in `tptp4X -c -x -umachine $theory | cut -f 1 -d ',' | sed -e 's/fof(//'`; do
+    grep --silent "label($formula)" $prover9_proof > /dev/null 2>&1;
     if [ $? -ne "0" ]; then echo $formula; fi
 done
