af993d3fb2454bc639aca7dc632f309316551d64
Jesse Alama <jesse.alama@gmail.com>
2011-10-23 18:10:16 +0200

    don't trust

--- run-prover9.sh
+++ run-prover9.sh
@@ -6,4 +6,8 @@ timeout=$2;
 ulimit -t $timeout \
     || (echo "Error: '$timeout' is not an acceptable argument to ulimit -t." && exit 1);
 
+# This transformation using tptp_to_ladr is not logically correct.  We
+# need to use something like tptp2X here, not tptp_to_ladr.  Until we
+# fix this problem, do not trust the output of this script.
+
 tptp_to_ladr < $theory | prover9 -x 2> /dev/null | prooftrans renumber
