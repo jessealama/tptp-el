642a0ee773f79154398543a3f1f8769b8a6e9c78
Jesse Alama <jesse.alama@gmail.com>
2011-10-24 22:06:31 +0200

    better eprover script

--- run-eprover.sh
+++ run-eprover.sh
@@ -16,4 +16,5 @@ fi
 ulimit -t $timeout \
     || (echo "Error: '$timeout' is not an acceptable argument to ulimit -t." && exit 1);
 
-eproof --tptp3-in $theory;
+eprover -l4 -xAuto -tAuto -R --memory-limit=1000 --tptp3-in $theory \
+    | epclextract
