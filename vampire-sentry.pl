d4847b2c5cd446baca1c83619ac651964add1e2e
Jesse Alama <jesse.alama@gmail.com>
2011-11-09 14:42:01 +0100

    don't launch echo; just print directly from the perl process

--- vampire-sentry.pl
+++ vampire-sentry.pl
@@ -10,22 +10,12 @@
 
 use strict;
 
-# launch epclextract
-
-open (ECHO, '|echo')
-  or (print STDERR 'Error: we are unable to open a pipe to echo.' && exit 1);
-
 while (defined (my $line = <STDIN>)) {
   if ($line =~ /Termination reason: Satisfiable/) {
-    close ECHO
-      or (print STDERR 'Error: our problem is countersatisfiable, so we wish to close our pipe to echo, but we cannot.' && exit 1);
     exit 2;
   } else {
-    print ECHO ($line);
+    print $line;
   }
 }
-
-close ECHO
-  or (print STDERR 'Error: we are unable to close the pipe to echo.' && exit 1);
 
 exit 0;
