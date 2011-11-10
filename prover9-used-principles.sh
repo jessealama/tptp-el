cc53dea4a91bef1b8145fe70f0373fbd3dc1f17d
Jesse Alama <jesse.alama@gmail.com>
2011-11-10 16:19:39 +0100

    Simplify extraction of prover9 used principles.

--- prover9-used-principles.sh
+++ prover9-used-principles.sh
@@ -13,36 +13,13 @@ if [ -z $theory ]; then
     exit 1;
 fi
 
-# Working with a prover9 output line such as
-#
-# 10 (all A all B (r1_xboole_0(A,B) <-> k3_xboole_0(A,B) = k1_xboole_0)) # label(d7_xboole_0) # label(definition) # label(non_clause).  [assumption].
-#
-# or
-#
-# 21 -r1_xboole_0(c1_14__xboole_0,c2_14__xboole_0) # label(e1_14_1__xboole_0) # label(assumption) # answer(e1_14_1__xboole_0).  [assumption].
-#
-# corresponding to an assumption, extract the labels and answers.
-
-enumerate_labels_awk_script='BEGIN { FS = "#"; } /./ { for (i = 2; i <= NF; i++) { print $i; } }';
-#                                    ^^ field separator     ^^^^^ start at field 2 because field 1 is everything before the first label
-
-function prover9_assumptions() {
-    local prover9_proof=$1;
-    grep '[assumption]' $prover9_proof \
-	| sed -e 's/\[assumption\]//' \
-              -e 's/\.//g' \
-	      -e 's/\[.*\]//';
-}
-
 function prover9_labels_and_answers() {
-    local prover9_proof=$1;
-    prover9_assumptions $prover9_proof \
-	| gawk "$enumerate_labels_awk_script" \
+    grep --only-matching 'label([^(]*)' $1 \
 	| sed -e 's/label(\(.*\))/\1/' \
-	      -e 's/answer(\(.*\))/\1/' \
-	      -e 's/^ *//' -e 's/ *$//' \
 	| sort -u \
-	| uniq;
+	| uniq \
+	| grep --invert-match 'axiom';
+#                              ^^^^^ prover9 uses this internally as a label; it does not come from our problem
 }
 
 for principle in `prover9_labels_and_answers $prover9_proof`; do
