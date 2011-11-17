27f65bbbcc1dd57f71d969d9ccd0dbaaeffb123b
Jesse Alama <jesse.alama@gmail.com>
2011-11-17 02:16:00 +0100

    Multiple "AndLHS" and "AndRHS" can appear!

--- prover9-unused-principles.sh
+++ prover9-unused-principles.sh
@@ -26,12 +26,9 @@ fi
 for formula in `tptp4X -V -N -c -x -umachine $theory | grep --invert-match ',conjecture,' | cut -f 1 -d ',' | sed -e 's/fof(//'`; do
     grep --silent "label($formula)" $prover9_proof > /dev/null 2>&1;
     if [ $? -ne "0" ]; then
-        grep --silent "label(${formula}_AndLHS)" $prover9_proof > /dev/null 2>&1;
+        grep --silent "label(${formula}(_AndLHS|_AndRHS)*)" $prover9_proof > /dev/null 2>&1;
         if [ $? -ne "0" ]; then
-            grep --silent "label(${formula}_AndRHS)" $prover9_proof > /dev/null 2>&1;
-            if [ $? -ne "0" ]; then
-                echo $formula;
-            fi
+            echo $formula;
         fi
     fi
 done
